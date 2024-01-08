aaa749f56afa5c4de88250e561a1b50a084195bd
steamdollar <sila@DESKTOP-KF4LRVA>
2024-01-08 22:19:47 +0900

    submit

--- etc/14278.go
+++ etc/14278.go
@@ -1,6 +1,67 @@
 /*
+	먼저 말씀드리자면 풀지 못했습니다.
+	다만 어떤 사고 과정을 거쳐 어디까지 해결을 했는지라도 보여드리기 위해 제출합니다.
+	코드 뒤에 사고 과정을 서술해 두었습니다.
+	괜찮으시다면 봐주세요.
+	감사합니다.
+*/
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func solution(w,h int) int {
+	
+	if w == 1 {
+		return (h+1) % 1000000007
+	}
+	
+	if w == 2 {
+		
+		ans := 6 * int(math.Pow(2, float64(h))) - 2*h - 5
+		return ans % 1000000007
+	}
+	
+	// solution(w,h) 값을 저장할 w * h 배열
+	memo := make([][]int, w)
+	
+	for i := 0; i < w; i++ {
+		memo[i] = make([]int, h)
+	}
+	
+	// f(1,h) = h + 1
+	for i := 0; i < h ; i++ {
+		memo[0][i] = i+2 
+ 	}
+	
+	memo[1][0] = 5
+	
+	// f(3, 1) = 13
+	memo[2][0] = 13
+	
+	for i := 0; i < h; i++ {
+		memo[2][i] = 6 * int(math.Pow(2, float64(i))) - 2*i - 5
+	}
+
+	return 0
+}
+
+func main() {
+        var w,h int
+        fmt.Scanf("%d %d", &w, &h)
+        
+	ans := solution(w, h)
+	
+        fmt.Println(ans)
+}
+
+/*
 동적 프로그래밍, 재귀를 사용해야할 것 같음.
-일단 작은 걸로 시작해, 큰걸 구하자.
+일단 작은 걸로 시작해, 큰 걸 구하자.
+
 변수는 base block의 width, 전체 블럭의 height.
 여기서 width는 상수로 주어진다.
 그래도 1에서 w까지 도달하는데 어차피 필요할 것 같음.
@@ -10,7 +71,7 @@ f(w,h) = f(?,?) + ... (1 <= w, h <= 10)
 플레 문제지만 어려운 cs 지식이 들어가는 것 같지 않고
 점화식 들어가는 수학 문제에 가까운 것 같다.
 
-각 base block의 넓이 = 1, 2... 의 경우를 메모이제이션해두면 연산도 많지 않게 될 것 같음.
+각 base block의 넓이 = 1, 2... 의 경우를 메모이제이션해두면 연산도 많지 않게 될 것 같다.
 
 결국 base = 1, 2, 3의 경우의 조합으로 환원될 것.
 
@@ -62,11 +123,14 @@ f(2,n) = g(2,1) + g(2,2) + ... g(2, n)
 
 //
 
-w = 3 인 경우부터는 브릿지되는 경우가 있어서 더 복잡한데 아마 이 때부턴 일반화를 해야할 것 같다.
+w = 3 인 경우부터는 브릿지되는 경우가 있어서 더 복잡한데 아마 이 때부턴 일반화를 해야하거나
+
+재귀적으로 어떻게 쪼갤 수 있는지 봐야할 것 같다.
 
 f(3,n) = g(3,1) + ... g(3, n)
 
 g(3,1) = f(3,1) = 13
+
 
 g(3,n) = (a) (n-1)층이 세 칸을 다 쓴 경우 = g(3, n-1)
 
@@ -79,56 +143,8 @@ g(3,n) = (a) (n-1)층이 세 칸을 다 쓴 경우 = g(3, n-1)
 
 	(c) 두 칸을 한 칸 띄고 쓴 경우 = c(3, n-1) * 3
 	(d) 한 칸만 쓴 경우 = d(3, n-1) * 1
+
+//
+
+w = 4 인 경우부터는 아마 w=3, 2, 1인 경우의 조합으로 나타날 듯..
 */
-package main
-
-import (
-	"fmt"
-	"math"
-)
-
-func solution(w,h int) int {
-	
-	if w == 1 {
-		return h+1
-	}
-	
-	if w == 2 {
-		ans := 6 * int(math.Pow(2, float64(h))) - 2*h - 6
-		return 1000000007 % ans
-	}
-	
-	// solution(w,h) 값을 저장할 w * h 배열
-	memo := make([][]int, w)
-	
-	for i := 0; i < w; i++ {
-		memo[i] = make([]int, h)
-	}
-	
-	// f(1,h) = h + 1
-	for i := 0; i < h ; i++ {
-		memo[0][i] = i+2 
- 	}
-	
-	memo[1][0] = 5
-	
-	// f(3, 1) = 13
-	memo[2][0] = 13
-	
-	for i := 0; i < h; i++ {
-		memo[2][i] = 6 * int(math.Pow(2, float64(i))) - 2*i - 6
-	}
-	
-	fmt.Println(memo)
-
-	return 0
-}
-
-func main() {
-        var w,h int
-        fmt.Scanf("%d %d", &w, &h)
-        
-	ans := solution(w, h)
-	
-        fmt.Println(ans)
-}
